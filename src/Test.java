182c3d07f8b7f0b36e727f71d9fd5522b390e1f1
MrDXY <xinyuandu@bupt.edu.cn>
2018-11-11 16:44:15 +0800

    增加Father类和son类方法调用

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,20 @@
+class father{
+    public father() {
+        System.out.println("This is Father");
+    }
+}
+
+class son extends father{
+    public son() {
+        System.out.println("This is Son");
+    }
+    public void say() {
+        System.out.println("父类调用子类成员函数");
+    }
+}
+
 public class Test {
-    Integer a1 = new Integer(0);
-    Integer a2 = new Integer(0);
-    static final StringBuffer s = new StringBuffer("Hello");
-    public static StringBuffer testStatic(final StringBuffer tmp){
-        tmp.append(" world");
-        System.out.println(tmp);
-        return tmp;
-    }
-
-    public static int getLength(final StringBuffer sb){
-        sb.append(" World");
-        return sb.length();
-
-    }
-
     public static void main(String[] args){
-        Test test = new Test();
-        String a = "abc";
-        String b = "abc";
-        String c = new String("abc");
-        System.out.println(a == b);
-        System.out.println(a == c);
-        System.out.println(a.hashCode());
-        System.out.println(c.hashCode());
-        StringBuffer sb = new StringBuffer("Hello");
-        System.out.println(getLength(sb));
+        father f = new son();
     }
 }
